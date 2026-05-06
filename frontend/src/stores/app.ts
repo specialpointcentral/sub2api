9a0ca41af24d9e5d7f70c90baf849050dde9a77c
Qi HU <github@spcsky.com>
2026-05-06 15:28:25 +0800

    feat: add user timezone preferences

--- frontend/src/stores/app.ts
+++ frontend/src/stores/app.ts
@@ -359,6 +359,7 @@ export const useAppStore = defineStore('app', () => {
         available_channels_enabled: false,
         risk_control_enabled: false,
         affiliate_enabled: false,
+        server_timezone: 'UTC',
       }
     }
 
