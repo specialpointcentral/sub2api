2e04f173a180dea346ba71099b8cadb567809eb7
Qi HU <github@spcsky.com>
2026-05-06 15:28:25 +0800

    feat: add user timezone preferences

--- backend/internal/handler/setting_handler.go
+++ backend/internal/handler/setting_handler.go
@@ -120,6 +120,7 @@ func (h *SettingHandler) GetPublicSettings(c *gin.Context) {
 		RiskControlEnabled: settings.RiskControlEnabled,
 
 		AllowUserViewErrorRequests: settings.AllowUserViewErrorRequests,
+		ServerTimezone:     settings.ServerTimezone,
 	})
 }
 
