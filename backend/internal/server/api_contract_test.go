ae8055827a53078ee89ffacee37d6a1777634850
Qi HU <github@spcsky.com>
2026-05-06 17:02:00 +0800

    feat: add billing statement emails

--- backend/internal/server/api_contract_test.go
+++ backend/internal/server/api_contract_test.go
@@ -67,6 +67,9 @@ func TestAPIContracts(t *testing.T) {
 					"balance_notify_extra_emails": null,
 					"total_recharged": 0,
 					"timezone": "UTC",
+					"billing_statement_daily_enabled": false,
+					"billing_statement_weekly_enabled": false,
+					"billing_statement_monthly_enabled": false,
 					"linuxdo_bound": false,
 					"oidc_bound": false,
 					"wechat_bound": false,
@@ -988,6 +991,7 @@ func TestAPIContracts(t *testing.T) {
 					"balance_low_notify_threshold": 0,
 					"balance_low_notify_recharge_url": "",
 					"account_quota_notify_emails": [],
+					"billing_statement_email_config": "",
 					"channel_monitor_enabled": true,
 					"channel_monitor_mode": "v1",
 					"channel_monitor_hide_throughput": true,
@@ -1301,6 +1305,7 @@ func TestAPIContracts(t *testing.T) {
 					"balance_low_notify_threshold": 0,
 					"balance_low_notify_recharge_url": "",
 					"account_quota_notify_emails": [],
+					"billing_statement_email_config": "",
 					"channel_monitor_enabled": true,
 					"channel_monitor_mode": "v1",
 					"channel_monitor_hide_throughput": true,
@@ -2840,6 +2845,11 @@ func (r *stubUsageLogRepo) GetStatsWithFilters(ctx context.Context, filters usag
 		Endpoints:                []usagestats.EndpointStat{},
 	}, nil
 }
+
+func (r *stubUsageLogRepo) GetBillingStatementLines(ctx context.Context, userID int64, startTime, endTime time.Time) ([]service.BillingStatementLine, error) {
+	return nil, errors.New("not implemented")
+}
+
 func (r *stubUsageLogRepo) GetAllGroupUsageSummary(ctx context.Context, todayStart time.Time) ([]usagestats.GroupUsageSummary, error) {
 	return nil, errors.New("not implemented")
 }
