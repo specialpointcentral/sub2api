9227050677f60dce5ef474714cc30d4404ecaef2
Qi HU <github@spcsky.com>
2026-05-09 13:17:30 +0800

    feat: add Kiro platform support

--- backend/internal/handler/gateway_handler_warmup_intercept_unit_test.go
+++ backend/internal/handler/gateway_handler_warmup_intercept_unit_test.go
@@ -165,6 +165,8 @@ func newTestGatewayHandler(t *testing.T, group *service.Group, accounts []*servi
 		nil, // httpUpstream
 		nil, // deferredService
 		nil, // claudeTokenProvider
+		nil, // kiroTokenProvider
+		nil, // kiroCooldownStore
 		nil, // sessionLimitCache
 		nil, // rpmCache
 		nil, // digestStore
