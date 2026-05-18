a0a1397bd36a61504ca507400b56d8f2383e2e35
Qi HU <github@spcsky.com>
2026-05-18 10:56:03 +0800

    fix: show ops request user and body

--- backend/internal/handler/openai_gateway_handler.go
+++ backend/internal/handler/openai_gateway_handler.go
@@ -221,6 +221,7 @@ func (h *OpenAIGatewayHandler) Responses(c *gin.Context) {
 		h.errorResponse(c, http.StatusBadRequest, "invalid_request_error", "Failed to read request body")
 		return
 	}
+	service.SetOpsRequestBody(c, body)
 
 	if len(body) == 0 {
 		h.errorResponse(c, http.StatusBadRequest, "invalid_request_error", "Request body is empty")
@@ -847,6 +848,7 @@ func (h *OpenAIGatewayHandler) Messages(c *gin.Context) {
 		h.anthropicErrorResponse(c, http.StatusBadRequest, "invalid_request_error", "Failed to read request body")
 		return
 	}
+	service.SetOpsRequestBody(c, body)
 	if len(body) == 0 {
 		h.anthropicErrorResponse(c, http.StatusBadRequest, "invalid_request_error", "Request body is empty")
 		return
